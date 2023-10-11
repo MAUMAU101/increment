d0775ff16061ca8cbda6349b6129cb548947d297
MAUMAU101 <40014287+MAUMAU101@users.noreply.github.com>
2023-10-10 21:21:30 -0700

    rest button added

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,51 +1,63 @@
 import 'package:flutter/material.dart';
 
 class Counter extends StatefulWidget {
-  const Counter({super.key});
+  const Counter({Key? key}) : super(key: key);
 
   @override
-  State<Counter> createState() => _CounterState();
+  _CounterState createState() => _CounterState();
 }
 
 class _CounterState extends State<Counter> {
   int clickCounter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$clickCounter',
-                style:
-                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
-              ),
-              const Text('Clicks', style: TextStyle(fontSize: 60))
-            ],
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    clickCounter += 1; //Icons.exposure_minus_1
-                  });
-                },
-                child: const Icon(Icons.plus_one)),
-            const SizedBox(height: 18),
-            FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    if (clickCounter > 0) {
-                      clickCounter --;
-                    }
-                  });
-                },
-                child: const Icon(Icons.exposure_minus_1)),
+            Text(
+              '$clickCounter',
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            const Text('Clicks', style: TextStyle(fontSize: 60)),
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter += 1;
+              });
+            },
+            child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 18),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                if (clickCounter > 0) {
+                  clickCounter--;
+                }
+              });
+            },
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+          const SizedBox(height: 18),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+            child: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
   }
 }
