b9bd9cd776a1291bfd235d1f4116acb3e4e3c4d7
MAUMAU101 <40014287+MAUMAU101@users.noreply.github.com>
2023-10-10 19:31:03 -0700

    First Commit

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int clickCounter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              const Text('Clicks', style: TextStyle(fontSize: 60))
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    clickCounter += 1; //Icons.exposure_minus_1
+                  });
+                },
+                child: const Icon(Icons.plus_one)),
+            const SizedBox(height: 18),
+            FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    clickCounter -= 1; //Icons.exposure_minus_1
+                    if (clickCounter <= 0) {
+                      clickCounter = 0;
+                    }
+                  });
+                },
+                child: const Icon(Icons.exposure_minus_1)),
+          ],
+        ));
+  }
+}
